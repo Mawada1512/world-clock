f78255e9c7525ad7dd34db57cc5a194bbb992e85
Mawada1512811 <mawadabeedy5@gmail.com>
2025-12-28 20:34:57 +0200

    add date and time for the selection cities

--- insex.js
+++ insex.js
@@ -1,19 +1,53 @@
 function updateTime() {
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDate = tokyoElement.querySelector(".date");
-  let tokyoTime = tokyoElement.querySelector(".time");
-  let tokyoCurrent = moment().tz("Asia/Tokyo");
-  tokyoDate.innerHTML = tokyoCurrent.format("MMMM Do, YYYY");
-  tokyoTime.innerHTML = tokyoCurrent.format("h:mm:ss [<small>]A[</small>]");
+  if (tokyoElement) {
+    let tokyoDate = tokyoElement.querySelector(".date");
+    let tokyoTime = tokyoElement.querySelector(".time");
+    let tokyoCurrent = moment().tz("Asia/Tokyo");
+    tokyoDate.innerHTML = tokyoCurrent.format("MMMM Do, YYYY");
+    tokyoTime.innerHTML = tokyoCurrent.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   let hongKongElement = document.querySelector("#hong-kong");
-  let hongKongDate = hongKongElement.querySelector(".date");
-  let hongKongTime = hongKongElement.querySelector(".time");
-  let hongKongCurrent = moment().tz("Asia/Hong_Kong");
-  hongKongDate.innerHTML = hongKongCurrent.format("MMMM Do, YYYY");
-  hongKongTime.innerHTML = hongKongCurrent.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (hongKongElement) {
+    let hongKongDate = hongKongElement.querySelector(".date");
+    let hongKongTime = hongKongElement.querySelector(".time");
+    let hongKongCurrent = moment().tz("Asia/Hong_Kong");
+    hongKongDate.innerHTML = hongKongCurrent.format("MMMM Do, YYYY");
+    hongKongTime.innerHTML = hongKongCurrent.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let khartoumElement = document.querySelector("#khartoum");
+  if (khartoumElement) {
+    let khartoumDate = khartoumElement.querySelector(".date");
+    let khartoumTime = khartoumElement.querySelector(".time");
+    let khartoumCurrent = moment().tz("Africa/Khartoum");
+    khartoumDate.innerHTML = khartoumCurrent.format("MMMM Do, YYYY");
+    khartoumTime.innerHTML = khartoumCurrent.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small> ${moment().format("A")}</small></div>
+        </div>`;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+let cityElement = document.querySelector("#city");
+cityElement.addEventListener("change", updateCity);
